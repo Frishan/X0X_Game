90ecce8a28593b391271dc9c870454a5b30d3a26
Frishan <frisanlaurentiu2@gmail.com>
2023-05-31 09:03:14 +0300

    Proiect LP

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 from tkinter import *
+#importam libraria tkinter pentru a folosi interfata grafica
 import random
+#importam libraria random pt a folosi functia random
 def new_game():
 
     global player
 
     player = random.choice(players)
 
+    #randul jucatorului
     label.config(text=player+" turn")
-
+    
+#creearea grid-ului de butoane si colorate in negru
     for row in range(3):
         for column in range(3):
             buttons[row][column].config(text="",bg="black")
@@ -18,10 +22,12 @@ window = Tk()
 window.title("XOX_Game")
 width= window.winfo_screenwidth()
 height= window.winfo_screenheight()
-#setting tkinter window size
+#resize la fereastra
 window.geometry("%dx%d" % (width, height))
+#alegere caractere jucatori si functia random pentru a incepe
 players = ["X" , "O"]
 player = random.choice(players)
+
 buttons = [[0,0,0],
            [0,0,0],
            [0,0,0]]
@@ -29,6 +35,7 @@ buttons = [[0,0,0],
 label = Label(text=player + " turn", font=('consolas',40) )
 label.pack(padx= 0 , pady= 30 ,side="top")
 
+#realizarea butonului de reset
 reset_button = Button(text="RESET", font=('consolas',20), command = new_game)
 reset_button.pack(pady= 30 , side="bottom")
 
@@ -39,11 +46,14 @@ def next_turn(row, column):
 
     global player
 
+#testare initiala pentru a vedea daca jocul nu este castigat
     if buttons[row][column]['text'] == "" and check_winner() is False:
 
         if player == players[0]:
 
             buttons[row][column]['text'] = player
+
+#schibarea jucatorului in caz ca jocul nu e castigat
 
             if check_winner() is False:
                 player = players[1]
@@ -71,12 +81,15 @@ def next_turn(row, column):
 
 def check_winner():
 
-    for row in range(3):
+#parcurgem fiecare rand sa vedem daca textul din buton daca e identic
+    for i in range(3):
         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
             buttons[row][0].config(bg="green")
             buttons[row][1].config(bg="green")
             buttons[row][2].config(bg="green")
             return True
+
+#parcurgem fiecare coloana sa vedem daca textul din buton daca e identic
 
     for column in range(3):
         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
@@ -84,6 +97,8 @@ def check_winner():
             buttons[1][column].config(bg="green")
             buttons[2][column].config(bg="green")
             return True
+
+#parcurgem fiecare diagonala sa vedem daca textul din buton daca e identic
 
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
         buttons[0][0].config(bg="green")
@@ -97,7 +112,10 @@ def check_winner():
         buttons[2][0].config(bg="green")
         return True
 
+#daca una din if urile de mai sus sunt indepicite butoanele respective se coloreaza in verde
     elif empty_spaces() is False:
+
+#in caz ca nu mai sunt spatii libere si nici un if nu e indeplinit e egalitate
 
         for row in range(3):
             for column in range(3):
@@ -116,11 +134,12 @@ def empty_spaces():
         for column in range(3):
             if buttons[row][column]['text'] != "":
                 spaces -= 1
-
+#pornim cu 9 spatii libere si in cand un buton este ocupat cu text scadem cate unu
     if spaces == 0:
         return False
     else:
         return True
+
 
 
 for row in range(3):
@@ -130,4 +149,3 @@ for row in range(3):
         buttons[row][column].grid(row=row,column=column)
 
 window.mainloop()
-
